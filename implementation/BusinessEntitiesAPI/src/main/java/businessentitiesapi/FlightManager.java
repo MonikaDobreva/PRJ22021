868ccee03601e937c27394ee31198c3f75474622
Bheniamyn <70885399+Bheniamyn@users.noreply.github.com>
2021-03-24 17:15:47 +0100

    Add modules

--- implementation/BusinessEntitiesAPI/src/main/java/businessentitiesapi/FlightManager.java
+++ implementation/BusinessEntitiesAPI/src/main/java/businessentitiesapi/FlightManager.java
@@ -12,23 +12,27 @@ public interface FlightManager {
 
     /**
      * Creates a new flight, fed with the name, departure and arrival date.
-     * @param name The name of the flight
-     * @param depTime The date and time of the departure of the flight
-     * @param arrTime The date and time of the arrival of the flight
-     * @param airplane The airplane used for the flight
+     *
+     * @param name         The name of the flight
+     * @param depTime      The date and time of the departure of the flight
+     * @param arrTime      The date and time of the arrival of the flight
+     * @param airplane     The airplane used for the flight
      * @param startAirport The airport where the flight takes off
-     * @param destAirport The airport where the flight arrives
+     * @param destAirport  The airport where the flight arrives
      * @return a new flight object
      */
-    Flight createFlight(String name,
-                        ZonedDateTime depTime,
-                        ZonedDateTime arrTime,
-                        String airplane,
-                        String startAirport,
-                        String destAirport);
+    Flight createFlight(
+            String name,
+            ZonedDateTime depTime,
+            ZonedDateTime arrTime,
+            String airplane,
+            String startAirport,
+            String destAirport
+    );
 
     /**
      * Adds a new flight to the FlightManager, which stores all the flight objects in a list
+     *
      * @param f The flight to be added
      * @return the added flight
      */
