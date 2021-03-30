7d10a33aa7fbe887a20fa7ada32dc9f16014d80b
Rachel <70886912+RDX8@users.noreply.github.com>
2021-03-30 15:18:21 +0200

    added airplane interface

---
+++ implementation/BusinessEntitiesAPI/src/main/java/businessentitiesapi/Airplane.java
@@ -0,0 +1,36 @@
+
+package businessentitiesapi;
+
+/**
+ * This represents the airplanes in the company
+ * @author Rachel
+ */
+public interface Airplane {
+    
+    /**
+     * This should just return this Airplane object.
+     * @return this object as Airplane type
+     */
+    Airplane getAirplane();
+    
+    /**
+     * Gives the code of the Airplane in the format A-BCDE
+     * @return Airplane Code
+     */
+    String getCode();
+    
+    /**
+     * Gives the Airplane name like Boeing 377
+     * @return AirplaneName
+     */
+    String getName();
+    
+    /**
+     * Gives the number of seats in this Airplane
+     * @return seat amount
+     */
+    int getSeatAmount();
+    
+    
+    
+}
