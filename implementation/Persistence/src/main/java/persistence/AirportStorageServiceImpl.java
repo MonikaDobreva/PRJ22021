9e801dd9cb55ec3739ece50a26e691a274947c92
AllexBoening <70882774+AllexBoening@users.noreply.github.com>
2021-03-31 14:58:59 +0200

    AirportStorageServiceImpl
    to be continued

--- implementation/Persistence/src/main/java/persistence/AirportStorageServiceImpl.java
+++ implementation/Persistence/src/main/java/persistence/AirportStorageServiceImpl.java
@@ -1,4 +1,34 @@
 package persistence;
 
+import businessentitiesapi.Airport;
+import businessentitiesapi.Flight;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
 public class AirportStorageServiceImpl {
+
+    private static List<Airport> airports = new ArrayList<>();
+
+    private Airport createAirport(String[] s) {
+        return null; // to be continued
+    }
+
+
+    public List<Airport> getAll() {
+        List<Flight> flights = new ArrayList<>();
+
+        try {
+            Files.lines(Path.of("airportStorage.csv"))
+                    .map(line -> line.split(","))
+                    .map(this::createAirport)
+                    .forEach(flights::add);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        return null; // to be continued
+    }
 }
