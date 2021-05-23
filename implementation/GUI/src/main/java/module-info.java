cd222c04d397aae18357afe0410aee867ca562df
Rachel <70886912+RachelD8@users.noreply.github.com>
2021-05-23 19:00:10 +0200

    add module info

--- implementation/GUI/src/main/java/module-info.java
+++ implementation/GUI/src/main/java/module-info.java
@@ -3,7 +3,7 @@ module GUI_module {
     requires transitive javafx.fxml;
     requires businessentities_api_module;
     requires transitive businesslogic_module;
-
+    requires java.logging;
     opens frontend to javafx.fxml;
     exports frontend;
 }
