4d99a2b176abcfd456c11cd406833332b5dd8190
Bheniamyn <70885399+Bheniamyn@users.noreply.github.com>
2021-05-24 22:58:29 +0200

    Add content to managementDashboardView.fxml

--- implementation/GUI/src/main/java/frontend/managementDashboardController.java
+++ implementation/GUI/src/main/java/frontend/managementDashboardController.java
@@ -1,13 +1,14 @@
 package frontend;
 
-import businessentitiesapi.AirplaneManager;
-import businessentitiesapi.AirportManager;
-import businessentitiesapi.FlightManager;
+import businessentitiesapi.*;
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 
 import java.util.function.Supplier;
+import java.util.stream.Collectors;
 
 /**
  * @author Benjamin Swiezy {@code b.swiezy@student.fontys.nl}
@@ -20,10 +21,13 @@ public class managementDashboardController {
     private final AirplaneManager airplaneManager;
 
     @FXML
-    Label totalFlightsLabel;
+    Label totalFlightsLabel, totalRoutesLabel, totalBookingsLabel, mDashDepLabel, mDashArrLabel, mDashEstFDLabel, mDashBookingsLabel, mDashMealsLabel;
 
     @FXML
     Button updateStatisticsButton;
+
+    @FXML
+    ComboBox<Integer> mDashFlightDropdown;
 
     public managementDashboardController(FlightManager flightManager, AirportManager airportManager, AirplaneManager airplaneManager){
         this.airplaneManager = airplaneManager;
@@ -34,6 +38,27 @@ public class managementDashboardController {
     @FXML
     private void updateData(){
         totalFlightsLabel.setText(String.valueOf((long) flightManager.getFlights().size()));
+        totalRoutesLabel.setText("Test");
+        totalBookingsLabel.setText("Test");
+    }
+
+    @FXML
+    private void listFlights(){
+        mDashFlightDropdown.setItems(FXCollections.observableArrayList(
+                flightManager.getFlights().stream()
+                        .map(Flight::getFlightID)
+                        .collect(Collectors.toList())));
+    }
+
+    @FXML
+    private void updateFlightData(){
+        var currentFlight = mDashFlightDropdown.getValue();
+        mDashDepLabel.setText("1");
+        mDashArrLabel.setText("2");
+        mDashEstFDLabel.setText("3");
+        mDashBookingsLabel.setText("4");
+        mDashMealsLabel.setText("5");
+
     }
 
 
