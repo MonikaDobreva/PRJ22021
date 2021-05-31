94e87ee9298450d81696f7aa68fd56f9a84fb3e6
Joel Sebastian <j.sebastian@student.fontys.nl>
2021-06-01 00:26:57 +0200

    Create classes for flight route entity

---
+++ implementation/BusinessEntitiesAPI/src/main/java/businessentitiesapi/FlightRouteManager.java
@@ -0,0 +1,38 @@
+package businessentitiesapi;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface FlightRouteManager {
+
+    /**
+     * Creates a new flight, fed with the name, departure and arrival date.
+     *
+     * @param flightRouteID             The name of the flight route
+     * @param originAirportCode       The airport where the flight takes off
+     * @param destinationAirportCode  The airport where the flight arrives
+     *
+     * @return a new flight object
+     */
+    FlightRoute createFlightRoute(
+            int flightRouteID,
+            String originAirportCode,
+            String destinationAirportCode
+    );
+
+    /**
+     * Adds a new flight to the FlightManager, which stores all the flight objects in a list
+     *
+     * @param fr The flight route to be added
+     * @return the added flight route
+     */
+    FlightRoute add(FlightRoute fr);
+
+    /**
+     * Get all flight routes
+     * @return a List of flight routes objects
+     */
+    List<FlightRoute> getFlightRoutes();
+
+}
