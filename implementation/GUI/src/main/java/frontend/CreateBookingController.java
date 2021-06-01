eb7153716aa5c91d1429acb39ffb593ccf5ac4c3
AllexBoening <70882774+AllexBoening@users.noreply.github.com>
2021-06-01 20:40:25 +0200

    fxml

--- implementation/GUI/src/main/java/frontend/CreateBookingController.java
+++ implementation/GUI/src/main/java/frontend/CreateBookingController.java
@@ -1,9 +1,28 @@
 package frontend;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ComboBox;
+
 import java.util.function.Supplier;
 
 public class CreateBookingController {
 
     public CreateBookingController(Supplier<SceneManager> sceneManager) {
     }
+
+    @FXML
+    ComboBox<String> selectFlight;
+
+    @FXML
+    CheckBox passengerOrNot;
+
+    @FXML
+    ComboBox<String> additionalPassenger;
+
+    @FXML
+    Button submitButton;
+
+
 }
