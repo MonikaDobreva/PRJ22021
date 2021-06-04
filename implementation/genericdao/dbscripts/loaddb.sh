dcb738d488468feb4249fa3262bba6350dc80224
Pieter van den Hombergh <pieter.van.den.hombergh@gmail.com>
2021-06-04 18:32:39 +0200

    you should not share solutions to prc2 exercises.

--- implementation/genericdao/dbscripts/loaddb.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-dbname=simpledao
-scriptdir=$(dirname $0)
-dropdb --if-exists ${dbname}
-createdb -O exam ${dbname}
-cat ${scriptdir}/schema.sql | psql -X ${dbname} 
-cat ${scriptdir}/newpiet.sql | psql -X ${dbname} 
