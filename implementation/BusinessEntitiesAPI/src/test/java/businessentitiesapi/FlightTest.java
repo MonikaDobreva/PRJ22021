e6f9265ac3fffbb64cafa6e3e3959cd7e0d72929
Rachel <70886912+RachelD8@users.noreply.github.com>
2021-06-04 23:05:52 +0200

    flight class tested

--- implementation/BusinessEntitiesAPI/src/test/java/businessentitiesapi/FlightTest.java
+++ implementation/BusinessEntitiesAPI/src/test/java/businessentitiesapi/FlightTest.java
@@ -3,11 +3,13 @@ package businessentitiesapi;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.time.Month;
+import java.util.List;
 import static org.assertj.core.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 import org.assertj.core.api.ThrowableAssert.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.assertj.core.api.SoftAssertions;
 
 /**
  *
@@ -73,4 +75,134 @@ public class FlightTest {
                 .isExactlyInstanceOf(IllegalArgumentException.class);
     }
 
+    @Test
+    public void getterTest() {
+        Flight f = new Flight(
+                1,
+                "DUS",
+                "LOD",
+                LocalDateTime.of(2022, Month.MARCH, 3, 13, 40),
+                LocalDateTime.of(2022, Month.MARCH, 3, 14, 40),
+                "A-AAAA",
+                new BigDecimal("30.00"));
+
+        SoftAssertions softly = new SoftAssertions();
+        softly.assertThat(f.getOriginAirport()).isEqualTo("DUS");
+        softly.assertThat(f.getDestinationAirport()).isEqualTo("LOD");
+        softly.assertThat(f.getDepartureTime()).isEqualTo(LocalDateTime.of(2022, Month.MARCH, 3, 13, 40));
+        softly.assertThat(f.getArrivalTime()).isEqualTo(LocalDateTime.of(2022, Month.MARCH, 3, 14, 40));
+        softly.assertThat(f.getAirplane()).isEqualTo("A-AAAA");
+        softly.assertThat(f.getBasePrice()).isEqualTo(new BigDecimal("30.00"));
+        softly.assertThat(f.getFlightID()).isEqualTo(1);
+
+        softly.assertAll();
+    }
+
+    @Test
+    public void toStringTest() {
+        Flight f = new Flight(
+                1,
+                "DUS",
+                "LOD",
+                LocalDateTime.of(2022, Month.MARCH, 3, 13, 40),
+                LocalDateTime.of(2022, Month.MARCH, 3, 14, 40),
+                "A-AAAA",
+                new BigDecimal("30.00"));
+
+        assertThat(f.toString()).contains(
+                List.of("1", "DUS", "LOD", "A-AAAA", "3", "13:40", "14:40"));
+    }
+
+    @Test
+    public void equalsHashTest() {
+        Flight ref = new Flight(
+                1,
+                "DUS",
+                "LOD",
+                LocalDateTime.of(2022, Month.MARCH, 3, 13, 40),
+                LocalDateTime.of(2022, Month.MARCH, 3, 14, 40),
+                "A-AAAA",
+                new BigDecimal("30.00"));
+
+        Flight equal = new Flight(
+                1,
+                "DUS",
+                "LOD",
+                LocalDateTime.of(2022, Month.MARCH, 3, 13, 40),
+                LocalDateTime.of(2022, Month.MARCH, 3, 14, 40),
+                "A-AAAA",
+                new BigDecimal("30.00"));
+
+        Flight unequal1 = new Flight(
+                2,
+                "DUS",
+                "LOD",
+                LocalDateTime.of(2022, Month.MARCH, 3, 13, 40),
+                LocalDateTime.of(2022, Month.MARCH, 3, 14, 40),
+                "A-AAAA",
+                new BigDecimal("30.00"));
+
+        Flight unequal2 = new Flight(
+                1,
+                "LAX",
+                "LOD",
+                LocalDateTime.of(2022, Month.MARCH, 3, 13, 40),
+                LocalDateTime.of(2022, Month.MARCH, 3, 14, 40),
+                "A-AAAA",
+                new BigDecimal("30.00"));
+
+        Flight unequal3 = new Flight(
+                1,
+                "DUS",
+                "LAX",
+                LocalDateTime.of(2022, Month.MARCH, 3, 13, 40),
+                LocalDateTime.of(2022, Month.MARCH, 3, 14, 40),
+                "A-AAAA",
+                new BigDecimal("30.00"));
+
+        Flight unequal4 = new Flight(
+                1,
+                "DUS",
+                "LOD",
+                LocalDateTime.of(2022, Month.FEBRUARY, 3, 13, 40),
+                LocalDateTime.of(2022, Month.MARCH, 3, 14, 40),
+                "A-AAAA",
+                new BigDecimal("30.00"));
+
+        Flight unequal5 = new Flight(
+                1,
+                "DUS",
+                "LOD",
+                LocalDateTime.of(2022, Month.MARCH, 3, 13, 40),
+                LocalDateTime.of(2024, Month.MARCH, 3, 14, 40),
+                "A-AAAA",
+                new BigDecimal("30.00"));
+
+        Flight unequal6 = new Flight(
+                1,
+                "DUS",
+                "LOD",
+                LocalDateTime.of(2022, Month.MARCH, 3, 13, 40),
+                LocalDateTime.of(2022, Month.MARCH, 3, 14, 40),
+                "A-AKLT",
+                new BigDecimal("30.00"));
+
+        Flight unequal7 = new Flight(
+                1,
+                "DUS",
+                "LOD",
+                LocalDateTime.of(2022, Month.MARCH, 3, 13, 40),
+                LocalDateTime.of(2022, Month.MARCH, 3, 14, 40),
+                "A-AAAA",
+                new BigDecimal("60.00"));
+
+        HelperEqualsHash.verifyEqualsAndHashCode(ref, equal,
+                unequal1,
+                unequal2,
+                unequal3,
+                unequal4,
+                unequal5,
+                unequal6,
+                unequal7);
+    }
 }
