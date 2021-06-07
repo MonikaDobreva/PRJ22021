fb9e48f0c1f708f8f2e536d14666866c07a62bc2
Rachel <70886912+RachelD8@users.noreply.github.com>
2021-06-07 09:33:34 +0200

    The flight manager test
    Not yet working fully but so that everyone has it

--- implementation/BusinessLogic/src/test/java/businesslogic/FlightManagerNewTest.java
+++ implementation/BusinessLogic/src/test/java/businesslogic/FlightManagerNewTest.java
@@ -1,4 +1,3 @@
-//
 //package businesslogic;
 //
 //import businessentitiesapi.Flight;
@@ -21,38 +20,49 @@
 //import static org.mockito.Mockito.mock;
 //
 ///**
+// * This tests that in the flight manager the dao is called with the belonging
+// * method. The dao is mocked to catch calls going there. The dao is fully
+// * tested.
 // *
 // * @author Rachel
 // */
 //public class FlightManagerNewTest {
-//    
+//
 //    @Mock
 //    PGDAOFactory daoM;
-//    
+//
 //    FlightManagerImpl flm;
 //    Flight f;
-//    
-//    
+//
 //    @BeforeEach
-//    public void setupTest(){
+//    public void setupTest() {
 //        flm = new FlightManagerImpl();
 //        daoM = mock(PGDAOFactory.class);
 //        flm.setDaoFactory(daoM);
-//        
+//
 //        f = new Flight(5,
-//            "DUS", "YVY",
-//            LocalDateTime.of(2021, 12, 3, 10, 30),
-//            LocalDateTime.of(2021, 12, 3, 15, 30),
-//            "V-BBBB",
-//            new BigDecimal("100.50"));
+//                "DUS", "YVY",
+//                LocalDateTime.of(2021, 12, 3, 10, 30),
+//                LocalDateTime.of(2021, 12, 3, 15, 30),
+//                "V-BBBB",
+//                new BigDecimal("100.50"));
 //    }
-//    
-////    @Disabled
+//
+//    @Disabled
 //    @Test
-//    public void updateTest(){
+//    public void updateTest() {
 ////        Mockito.when(daoM.createDao(forClass)).thenReturn(daoM);
 //        flm.update(f);
 //        verify(daoM).createDao(Flight.class).update(f);
-//        
+//
+//    }
+//    
+//    @Disabled
+//    @Test
+//    public void deleteTest() {
+////        Mockito.when(daoM.createDao(forClass)).thenReturn(daoM);
+//        flm.delete(f);
+//        verify(daoM).createDao(Flight.class).deleteEntity(f);
+//
 //    }
 //}
