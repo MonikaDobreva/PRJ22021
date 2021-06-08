457633855ede685aa898a46d49311a086d6120f7
Rachel <70886912+RachelD8@users.noreply.github.com>
2021-06-08 15:06:32 +0200

    edit Details Flight

--- implementation/BusinessLogic/src/test/java/businesslogic/EditDetailsLogicTest.java
+++ implementation/BusinessLogic/src/test/java/businesslogic/EditDetailsLogicTest.java
@@ -1,17 +1,22 @@
 package businesslogic;
 
+import businessentitiesapi.Flight;
 import businessentitiesapi.FlightManager;
 import businessentitiesapi.FlightRouteManager;
-import java.time.LocalDate;
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.Map;
 import static org.assertj.core.api.Assertions.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.mockito.Mock;
 import static org.mockito.Mockito.verify;
 import org.junit.jupiter.api.BeforeEach;
-import static org.mockito.Mockito.mock;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.api.Test;
+import static org.mockito.Mockito.mock;
+import org.mockito.Mockito;
 
 /**
  *
@@ -24,10 +29,21 @@ public class EditDetailsLogicTest {
     @Mock
     FlightRouteManager fRouteManagerM;
 
+    EditDetailsLogic el;
+    Flight f;
+
     @BeforeEach
     public void setupT() {
         fManagerM = mock(FlightManager.class);
         fRouteManagerM = mock(FlightRouteManager.class);
+        el = new EditDetailsLogic(fManagerM, fRouteManagerM);
+        f = new Flight(5,
+                "DUS", "YVY",
+                LocalDateTime.of(2021, 12, 3, 10, 30),
+                LocalDateTime.of(2021, 12, 3, 15, 30),
+                "V-BBBB",
+                new BigDecimal("100.50"));
+
     }
 
     /**
@@ -47,13 +63,122 @@ public class EditDetailsLogicTest {
         "12,3,2021-07-10,12:03 2021-07-10"
     })
     public void makeDateTimeTest(String hour, String min, String d, String expected) {
-        EditDetailsLogic el = new EditDetailsLogic(fManagerM,fRouteManagerM);
-        
-        
         LocalDateTime expectedDate = LocalDateTime.parse(
                 expected,
                 DateTimeFormatter.ofPattern("HH:mm yyyy-MM-dd"));
-
         assertThat(el.makeDateTime(hour, min, d)).isEqualTo(expectedDate);
     }
+
+    /**
+     * Flight is wrong, data rejected on the layer of the flightmanager map that
+     * is returned should contain flightError
+     */
+    @Test
+    public void passDataTest1() {
+        Mockito.when(fManagerM.createFlight(
+                Mockito.anyInt(),
+                Mockito.anyString(),
+                Mockito.anyString(),
+                Mockito.any(),
+                Mockito.any(),
+                Mockito.anyString(),
+                Mockito.any())).thenThrow(RuntimeException.class);
+
+        Map<String, String> savedValues = new HashMap<>();
+
+        savedValues.put("cVArrHour", "12");
+        savedValues.put("cVArrMin", "40");
+        savedValues.put("cVArrDate", "1999-04-12");
+
+        Map<String, String> returnValues = el.passData(savedValues, f);
+
+        verify(fManagerM).createFlight(
+                Mockito.anyInt(),
+                Mockito.anyString(),
+                Mockito.anyString(),
+                Mockito.any(),
+                Mockito.any(),
+                Mockito.anyString(),
+                Mockito.any());
+    
+        assertThat(returnValues).containsKey("flightError");
+    }
+    
+    /**
+     * Flight is right, update method returns false, map that
+     * is returned should contain updateError
+     */
+    @Test
+    public void passDataTest2() {
+        Mockito.when(fManagerM.createFlight(
+                Mockito.anyInt(),
+                Mockito.anyString(),
+                Mockito.anyString(),
+                Mockito.any(),
+                Mockito.any(),
+                Mockito.anyString(),
+                Mockito.any())).thenReturn(f);
+        
+        Mockito.when(fManagerM.update(f)).thenReturn(false);
+
+        Map<String, String> savedValues = new HashMap<>();
+
+        savedValues.put("cVArrHour", "12");
+        savedValues.put("cVArrMin", "40");
+        savedValues.put("cVArrDate", "2025-04-12");
+
+        Map<String, String> returnValues = el.passData(savedValues, f);
+
+        verify(fManagerM).createFlight(
+                Mockito.anyInt(),
+                Mockito.anyString(),
+                Mockito.anyString(),
+                Mockito.any(),
+                Mockito.any(),
+                Mockito.anyString(),
+                Mockito.any());
+        verify(fManagerM).update(f);
+    
+        assertThat(returnValues).containsKey("updateError");
+    }
+    
+    /**
+     * Flight is right, update method returns true, map that
+     * is returned should contain worked
+     */
+    @Test
+    public void passDataTest3() {
+        Mockito.when(fManagerM.createFlight(
+                Mockito.anyInt(),
+                Mockito.anyString(),
+                Mockito.anyString(),
+                Mockito.any(),
+                Mockito.any(),
+                Mockito.anyString(),
+                Mockito.any())).thenReturn(f);
+        
+        Mockito.when(fManagerM.update(f)).thenReturn(true);
+
+        Map<String, String> savedValues = new HashMap<>();
+
+        savedValues.put("cVArrHour", "12");
+        savedValues.put("cVArrMin", "40");
+        savedValues.put("cVArrDate", "2025-04-12");
+
+        Map<String, String> returnValues = el.passData(savedValues, f);
+
+        verify(fManagerM).createFlight(
+                Mockito.anyInt(),
+                Mockito.anyString(),
+                Mockito.anyString(),
+                Mockito.any(),
+                Mockito.any(),
+                Mockito.anyString(),
+                Mockito.any());
+        verify(fManagerM).update(f);
+    
+        assertThat(returnValues).containsKey("worked");
+    }
+
+   
 }
